d1a4d7ad6eeefdfcba899ffa50584c8df18d16cf
pnppl <pnpplpnppl@gmail.com>
2023-05-22 03:53:17 -0400

    Add files via upload

--- roku-remote.sh
+++ roku-remote.sh
@@ -6,15 +6,14 @@
 # TODO:
 # add utf8 to keyboard? need some depend for proper url encoding
 # ^ could improve search interface. could also add search operators, voice search (triggered by search keypress which isn't used right now)
-# improve behavior when holding key down/clean up terminal printing
 # backspace/esc key??? can use esc with escape character but then it gets triggered by mistake a lot
 
-### CONFIG: ###
-roku='192.168.0.12'
-###############
+###### CONFIG: ######
+roku='192.168.0.12' # also settable as sole arg or by pressing 'a'
+debug=false         # currently just controls if echo is on
+#####################
 
 bash44=false
-key='NULL'
 escape=$(printf "\u1b")
 
 # handle character input
@@ -37,19 +36,81 @@ getKey () {
   fi
 }
 
+noEcho () {  
+  if [[ $debug = false ]]; then
+    stty -echo
+  fi
+}
+
+debug () {
+  if [[ $debug = false ]]; then
+    debug=true
+    stty echo
+  else
+    debug=false
+    stty -echo
+  fi
+}
+
+# prints control info
+controls () {
+  F='\033[4m\033[1m' # bold, underline
+  B='\033[1m' # bold
+  FF='\033[0m' # clear
+  
+  echo -ne $B"arrows"$FF":arrows  "
+  echo -ne $B"\`"$FF":back  "
+  echo -ne $B"enter"$FF":OK-enter  "
+  echo -ne $B"Del"$FF":backspace  "
+  echo -ne $B"space"$FF":play-pause  "
+  echo -ne $B","$FF":rewind  "
+  echo -ne $B"."$FF":forward  "
+  echo -ne $B"-"$FF":vol-down  "
+  echo -ne $B"="$FF":vol-up  "
+  echo -ne $B"["$FF":channel-down  "
+  echo -ne $B"]"$FF":channel-up  "
+  echo
+  echo -ne $F"a"$FF"ddress  "
+  echo -ne $F"d"$FF"ebug  "
+  echo -ne $F"f"$FF"ind-remote  "
+  echo -ne $F"h"$FF"ome  "
+  echo -ne $F"i"$FF"nfo-*  "
+  echo -ne $F"k"$FF"eyboard-mode  "
+  echo -ne $F"m"$FF"ute  "
+  echo -ne $F"o"$FF"ff  " 
+  echo -ne $F"O"$FF"n  "
+  echo -ne $F"p"$FF"ower  "
+  echo -ne $F"r"$FF"eplay  "
+  echo -ne $F"s"$FF"earch  "
+  echo -ne $F"S"$FF"earch-inline  "
+  echo -ne $F"y"$FF"outube  "
+  echo
+  echo -ne $B"0"$FF":tuner  "
+  echo -ne "HDMI"$F"1"$FF"  "
+  echo -ne "HDMI"$F"2"$FF"  "
+  echo -ne "HDMI"$F"3"$FF"  "
+  echo -ne "HDMI"$F"4"$FF"  "
+  echo -ne $B"5"$FF":AV-in"
+  echo
+}
+
 # seamless search
 # no punctuation support
 search () {
+  stty echo
   read -ep 'search term: '
   term=${REPLY//' '/'+'}
   curl -gd '' "$roku:8060/search/browse?keyword=$term"
+  noEcho
 }
 
 # set IP interactively
 setIP () {
+  stty echo
   read -ep 'new IP: '
   roku=$REPLY
-  echo "controlling $roku"
+  echo "now controlling $roku"
+  noEcho
 }
 
 # handle onscreen keyboard
@@ -88,34 +149,25 @@ kb () {
       '[C') key='right' ;;
       '`')  key='back' ;;
       '[3~') key='backspace' ;; # Del key
-      '[2~') 
+      '[2~') # this isn't working consistently
         echo 'exiting onscreen keyboard mode' 
         return 0
       ;;
-      *) key='NULL' ;;
+      *) key='' ;;
     esac
     
-    if [[ $key != "NULL" ]]; then
+    if [[ $key != "" ]]; then
       curl -gd '' "$roku:8060/keypress/$key"
     fi
   done
-}
-
-# quick and dirty power toggle
-power () {
-  on=$(curl --silent "$roku:8060/query/device-info" | grep PowerOn)
-  if [[ $on == '<power-mode>PowerOn</power-mode>' ]]; then
-    curl -d '' "$roku:8060/keypress/powerOff"
-  else
-    curl -d '' "$roku:8060/keypress/powerOn"
-  fi
 }
 
 #### main ####
 roku=${1:-$roku} # set IP to first arg or fallback to config if no args
 echo '* Roku remote *'
 echo "controlling $roku"
-echo '(controls here)'
+echo 'c for full control list'
+noEcho # turn tty echo off if debug=false
 
 if [ ${BASH_VERSION:0:1} -ge 4 ] && [ ${BASH_VERSION:2:1} -ge 4 ]; then
   bash44=true
@@ -124,7 +176,7 @@ fi
 while :; do
   getKey
   
-  # change key to roku keypress string (or my control code)
+  # change key to roku keypress string to print, or do task and set to 
   case $REPLY in
     'h') key='home' ;;
     ',') key='rev' ;;
@@ -137,8 +189,9 @@ while :; do
     '-') key='volumeDown' ;;
     '=') key='volumeUp' ;;
     'm') key='volumeMute' ;;
-#    'o') key='powerOff' ;;
-#    'O') key='powerOn' ;; # make into toggle with /query/device-info
+    'o') key='powerOff' ;;
+    'O') key='powerOn' ;;
+    'p') key='power' ;;
     ']') key='channelUp' ;;
     '[') key='channelDown' ;;
     '0') key='inputTuner' ;;
@@ -147,29 +200,21 @@ while :; do
     '3') key='inputHDMI3' ;;
     '4') key='inputHDMI4' ;;
     '5') key='inputAV1' ;;
+    '`') key='back' ;;
     '[A') key='up' ;;
     '[B') key='down' ;;
     '[D') key='left' ;;
     '[C') key='right' ;;
-    '`') key='back' ;;
-    'k') key='ONSCREEN_KB' ;;
-    'o') key='POWER';;
-    's') key='SEARCH' ;;
-    'S') key='SEARCH_SEAMLESS' ;;
-    'a') key='IP' ;;
-    'y') key='YOUTUBE' ;;
-    *) key='NULL' ;;
+    'k') kb; key='' ;;
+    's') curl -d '' "$roku:8060/search/browse?keyword="; key='' ;; # go to search
+    'S') search; key='' ;;
+    'a') setIP; key='' ;;
+    'y') curl -d '' "$roku:8060/launch/837"; key='' ;; # youtube
+    'd') debug; key='' ;;
+    'c') controls; key='' ;;
+    *) key='' ;;
   esac
-    
-# send keypresses/call helpers
-  case $key in
-    'ONSCREEN_KB') kb ;;
-    'POWER') power ;;
-    'SEARCH') curl -d '' "$roku:8060/search/browse?keyword=" ;;
-    'SEARCH_SEAMLESS') search ;;
-    'IP') setIP ;;
-    'YOUTUBE') curl -d '' "$roku:8060/launch/837" ;;
-    'NULL') ;;
-    *) curl -gd '' "$roku:8060/keypress/$key" ;;
-  esac
+  if [[ -n $key ]]; then 
+    curl -gd '' "$roku:8060/keypress/$key" 
+  fi
 done
